b0c0dc850c6e7a198d22ca9147e70651f307e70a
Sam Morley <admin@inakleinbottle.com>
2025-06-13 12:51:14 +0100

    Comment out 4x4 GEMM-related code and increase `kNumMatrices` for benchmarks.

--- bench_config.cuh
+++ bench_config.cuh
@@ -4,6 +4,6 @@
 #ifndef BENCH_CONFIG_CUH
 #define BENCH_CONFIG_CUH
 
-static constexpr int64_t kNumMatrices = 1024 << 2;
+static constexpr int64_t kNumMatrices = 1024 << 5;
 
 #endif //BENCH_CONFIG_CUH
