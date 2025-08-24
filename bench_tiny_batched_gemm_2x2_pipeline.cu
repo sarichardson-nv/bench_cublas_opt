c68a08c5c3a825cdecc04bc965b00ea9218b8fb3
Sam Morley <admin@inakleinbottle.com>
2025-08-24 18:34:17 +0100

    Add 2x2 and pipelined 2x2 GEMM benchmarks and update CMakeLists for inclusion.

---
+++ bench_tiny_batched_gemm_2x2_pipeline.cu
@@ -0,0 +1,79 @@
+
+
+
+#include <benchmark/benchmark.h>
+#include <thrust/device_vector.h>
+
+#include "bench_config.cuh"
+#include "tiny_batched_gemm_pipeline.cuh"
+
+
+inline constexpr auto gemm_rm = tiny_batched_gemm_pipeline<float, 2, 256, Eigen::RowMajor>;
+inline constexpr auto gemm_cm = tiny_batched_gemm_pipeline<float, 2, 256, Eigen::ColMajor>;
+
+static void bench_tiny_batched_gemm_2x2_pipeline_rm(benchmark::State &state) {
+    constexpr int dim = 2;
+    constexpr int size = dim * dim;
+    const auto n_matrices = static_cast<int>(state.range(0));
+
+
+    thrust::device_vector<float> a(size * n_matrices);
+    thrust::device_vector<float> b(size * n_matrices);
+    thrust::device_vector<float> c(size * n_matrices);
+
+    for (auto _: state) {
+        const auto threads = 256;
+        const auto blocks = (n_matrices + threads - 1) / threads;
+
+        float alpha = 1.0;
+        float beta = 0.0;
+
+
+        gemm_rm<<<blocks, threads>>>(
+            raw_pointer_cast(a.data()),
+            raw_pointer_cast(b.data()),
+            raw_pointer_cast(c.data()),
+            alpha,
+            beta,
+            n_matrices);
+
+
+        cudaDeviceSynchronize();
+    }
+}
+
+BENCHMARK(bench_tiny_batched_gemm_2x2_pipeline_rm)->Arg(kNumMatrices);
+
+//
+// static void bench_tiny_batched_gemm_3x3_cm(benchmark::State &state) {
+//     constexpr int dim = 3;
+//     constexpr int size = dim * dim;
+//     const auto n_matrices = static_cast<int>(state.range(0));
+//
+//
+//     thrust::device_vector<float> a(size * n_matrices);
+//     thrust::device_vector<float> b(size * n_matrices);
+//     thrust::device_vector<float> c(size * n_matrices);
+//
+//     for (auto _: state) {
+//         const auto threads = 256;
+//         const auto blocks = (n_matrices + threads - 1) / threads;
+//
+//         float alpha = 1.0;
+//         float beta = 0.0;
+//
+//
+//         tiny_batched_gemm_3x3_cm<<<blocks, threads>>>(
+//             raw_pointer_cast(a.data()),
+//             raw_pointer_cast(b.data()),
+//             raw_pointer_cast(c.data()),
+//             alpha,
+//             beta,
+//             n_matrices);
+//
+//
+//         cudaDeviceSynchronize();
+//     }
+// }
+//
+// BENCHMARK(bench_tiny_batched_gemm_3x3_cm)->Arg(kNumMatrices);
